1f384b5197f434ee0a0f6f7e3fa17db57a50fbe6
echan <echan>
2013-02-09 23:49:40 +0000

    Updated the ArmHighShooting to raise the arm to the point in the arm subsystem

--- src/org/usfirst/frc330/Beachbot2013Java/commands/ArmHighShooting.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/ArmHighShooting.java
@@ -24,16 +24,20 @@ public class  ArmHighShooting extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.arm.holdArmOff();
+        Robot.arm.armSetPointHighShooting();
+        Robot.arm.enable();
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.arm.onTarget();
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.arm.holdArmOn();
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
