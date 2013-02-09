71d3676429e4e555ffe8f975b0eb158084b029cc
echan <echan>
2013-02-09 22:21:44 +0000

    Updated the Shift Low to shift to low.

--- src/org/usfirst/frc330/Beachbot2013Java/commands/ShiftLow.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/ShiftLow.java
@@ -26,10 +26,11 @@ public class  ShiftLow extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.chassis.shiftLow();
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
     // Called once after isFinished returns true
     protected void end() {
