461c60841ced70fecfd56d6d7ec672fcb3ab2616
jross <jross>
2013-02-18 20:48:13 +0000

    Make pickup frisbee commands finish immediately and don't make pickupUp the default command (could cause bad movement).

--- src/org/usfirst/frc330/Beachbot2013Java/commands/PickupFrisbeesOff.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/PickupFrisbeesOff.java
@@ -32,7 +32,7 @@ public class PickupFrisbeesOff extends Command implements AutoSpreadsheetCommand
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
     // Called once after isFinished returns true
     protected void end() {
