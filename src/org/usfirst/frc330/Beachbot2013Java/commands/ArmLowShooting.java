1a59f642590bc6252dcd73ee357e264d1bf9b34e
jross <jross>
2013-02-19 23:21:45 +0000

    whitespace changes from RobotBuilder

--- src/org/usfirst/frc330/Beachbot2013Java/commands/ArmLowShooting.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/ArmLowShooting.java
@@ -41,6 +41,7 @@ public class  ArmLowShooting extends Command implements AutoSpreadsheetCommand {
     // Called once after isFinished returns true
     protected void end() {
         Robot.arm.holdArmOn();
+        
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
