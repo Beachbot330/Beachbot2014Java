f4aa4595300256074672151d1a2a011fb30a1e73
jross <jross>
2013-02-27 06:03:02 +0000

    add javadoc

--- src/org/usfirst/frc330/Beachbot2013Java/commands/DriveWaypointBackward.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/DriveWaypointBackward.java
@@ -8,8 +8,30 @@ import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc330.Beachbot2013Java.Robot;
 
 /**
- *
- * @author joe
+ * Drive the robot backward to a specified waypoint (relative to where the robot started) 
+ * using the encoders and the gyro to keep straight and ramp up to full speed. Finish when
+ * one of the encoders is within the specified {@link #setParam3(double) tolerance}. 
+ * The {@link #setParam2(double) Y} location is positive to the front of the robot. 
+ * The {@link #setParam1(double) X} location is positive to the right of the robot.
+ * The robot's current angle should be close to the angle to drive to the waypoint
+ * (ie this command is not appropriate for both turning and driving).
+ * If the robot needs to turn prior to driving to the waypoint, use {@link TurnWaypoint} 
+ * first.
+ * <p>This is similar to DriveWaypoint, except that the robot drives backwards
+ * (pickup first).
+ * <p>
+ * For example, to drive 10 feet forward from where the robot started, set Y to 
+ * 120 (inches) and set X to 0.
+ * A reasonable tolerance is 3 inches for normal movements. This will stop the robot
+ * when it is between 117 - 123 inches. If a smaller
+ * tolerance is used, the robot may not ever reach the tolerance, and the 
+ * {@link #setTimeout(double) timeout} may be exceeded. This will slow down the
+ * execution of future commands.
+ * 
+ * @see DriveWaypoint
+ * @see TurnGyroWaypointBackward
+ * 
+ * @author Joe
  */
 public class DriveWaypointBackward extends DriveWaypoint {
 
