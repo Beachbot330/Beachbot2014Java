1e00f453e540ac6aa530e219a5696450b1fd6950
echan <echan>
2013-03-15 02:50:30 +0000

    added cvs log comments

--- src/org/usfirst/frc330/Beachbot2013Java/commands/ControlLEDs.java
+++ src/org/usfirst/frc330/Beachbot2013Java/commands/ControlLEDs.java
@@ -15,6 +15,10 @@ import edu.wpi.first.wpilibj.buttons.NetworkButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc330.Beachbot2013Java.Robot;
+/*
+ * $Log$
+ */
+
 /**
  *
  */
