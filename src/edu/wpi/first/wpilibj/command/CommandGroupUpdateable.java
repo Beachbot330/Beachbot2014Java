4a1a227428b661862e74bc25e2aa5d5020ebe0c0
Joe Ross <git@rossesmail.com>
2014-03-01 13:02:09 -0800

    Add CommandGroupUpdateable which nerfs the validate command. This allows commands to be added in initialize

---
+++ src/edu/wpi/first/wpilibj/command/CommandGroupUpdateable.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.command;
+
+/**
+ * Removes check when adding adding a Command to a CommandGroup. This allows
+ * Commands to be added in initialize.
+ * @author Joe-XPS13-W7
+ */
+public class CommandGroupUpdateable extends CommandGroup {
+
+    synchronized void validate(String message) {
+        //do nothing. 
+    }
+    
+}
