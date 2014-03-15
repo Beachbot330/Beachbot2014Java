cab72914042ce05c4e389ba0520280f25279c8a2
Allen <git@allenpeters.com>
2014-03-15 13:34:49 -0700

    Update auto spreadsheet for 2 ball auto (not complete)

--- src/org/usfirst/frc330/Beachbot2014Java/commands/AutoLoadShooter.java
+++ src/org/usfirst/frc330/Beachbot2014Java/commands/AutoLoadShooter.java
@@ -94,6 +94,7 @@ public class  AutoLoadShooter extends Command implements AutoSpreadsheetCommand{
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        System.out.println("autoLoadShooter Interrupted");
         end();
     }
     
