959c55a00fe1d04521cd743cced3cd7c838b55d6
Joe Ross <git@rossesmail.com>
2014-04-15 20:19:02 -0700

    add a conditional shooter eject using the Kinect, for 1 point hot goal

--- src/org/usfirst/frc330/Beachbot2014Java/Robot.java
+++ src/org/usfirst/frc330/Beachbot2014Java/Robot.java
@@ -135,6 +135,7 @@ public class Robot extends IterativeRobot {
         auto.addCommand(new KinectDrive());
         auto.addCommand(new RearPickupOn());
         auto.addCommand(new KinectConditionalShoot());
+        auto.addCommand(new KinectConditionalShooterEject());
         
         SmartDashboard.putNumber("KinectRightOffset", 0);
         SmartDashboard.putNumber("KinectLeftOffset", 0);
